ebf568ecefb4fe94d5708c51d629b3081d2c7e4c
Ze'ev Klapow <zklapow@willowgarage.com>
2012-12-28 11:08:10 -0800

    Motor widget is clicky.

--- src/turtlebot2_dashboard/motor_widget.py
+++ src/turtlebot2_dashboard/motor_widget.py
@@ -3,10 +3,10 @@ from functools import partial
 
 from kobuki_msgs.msg import MotorPower
 
-from rqt_robot_dashboard.widgets import MenuDashWidget
+from rqt_robot_dashboard.widgets import IconToolButton
 from python_qt_binding.QtCore import QSize
 
-class MotorWidget(MenuDashWidget):
+class MotorWidget(IconToolButton):
     def __init__(self, topic):
         self._pub = rospy.Publisher(topic, MotorPower) 
 
@@ -20,13 +20,18 @@ class MotorWidget(MenuDashWidget):
         #TODO: This is bad the state should be checked not set
         self.update_state(1)
 
-        self.add_action('Off', partial(self.update_state, 0))
-        self.add_action('On', partial(self.update_state, 1))
+        self.clicked.connect(self.toggle)
 
     def update_state(self, state):
         super(MotorWidget, self).update_state(state)
 
         self._pub.publish(MotorPower(state))
+
+    def toggle(self):
+        if self.state == 1:
+            self.update_state(0)
+        else:
+            self.update_state(1)
 
     def close(self):
         self._pub.unregister()
